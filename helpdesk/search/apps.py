4a6062634009b25c88022ce09f6bfa2721d9bcb4
Pritesh Ranjan <pranjan341@gmail.com>
2021-07-28 14:15:50 +0530

    updated readme

--- helpdesk/search/apps.py
+++ helpdesk/search/apps.py
@@ -4,17 +4,3 @@ from django.apps import AppConfig
 class SearchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search'
-
-    # def ready(self):
-    #     logging.info("generating fake data!")
-    #     fake = Faker()
-    #     keywords = ['help', 'ticket', 'support', 'data', 'tech', 'computer', 'hardware', 'software', 'dashboard',
-    #                 'rating',
-    #                 'install', 'working', 'admin', 'IT', 'supervisor', 'manager', 'error', 'missing', 'not installed',
-    #                 'not starting', 'failure', 'boot']
-    #     for _ in range(100):
-    #         title = fake.sentence(ext_word_list=keywords)
-    #         details = fake.sentence(ext_word_list=keywords)
-    #         owner = fake.name()
-    #         hd = HelpdeskModel(title=title.rstrip('.'), details=details, owner=owner)
-    #         hd.save()
