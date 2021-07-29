73e547827c15d6aeba84c1a0218af6f8d115fe71
Pritesh Ranjan <pranjan341@gmail.com>
2021-07-29 14:13:45 +0530

    Update settings.py

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
